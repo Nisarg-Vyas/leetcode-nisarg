7c1ff0a93c4be78d2701f6b810cb26a231aa93f8
Legitypus <thisisnisargvyas1922@gmail.com>
2026-03-17 00:01:53 +0530

    1275

---
+++ programmingSkills_StudyPlan/1275_findWinnerTicTacToe.cpp
@@ -0,0 +1,57 @@
+/*
+learnt:
+- matrix of vector (vector or vector)
+- dealing with vector of vector
+- good if else conditions
+- good question
+*/
+
+class Solution {
+public:
+    string tictactoe(vector<vector<int>>& moves) {
+        vector<vector<char>> board(3,vector<char>(3,' '));
+
+        for(int i=0;i<moves.size();++i){
+            int r=moves[i][0];
+            int c=moves[i][1];
+
+            if(i%2==0) board[r][c]='X';
+            else board[r][c]='O';
+        }
+
+        for(int i=0;i<3;++i){
+            if (board[i][0]==board[i][1] &&
+                board[i][1]==board[i][2] &&
+                board[i][0]!=' '){
+                    if(board[i][0]=='X') return "A";
+                    else return "B";
+                }
+        }
+        for(int i=0;i<3;++i){
+            if (board[0][i]==board[1][i] &&
+                board[1][i]==board[2][i] &&
+                board[0][i]!=' '){
+                    if(board[0][i]=='X') return "A";
+                    else return "B";
+                }
+        }
+        for(int i=0;i<3;++i){
+            if (board[0][0]==board[1][1] &&
+                board[1][1]==board[2][2] &&
+                board[0][0]!=' '){
+                    if(board[0][0]=='X') return "A";
+                    else return "B";
+                }
+        }
+        for(int i=0;i<3;++i){
+            if (board[0][2]==board[1][1] &&
+                board[1][1]==board[2][0] &&
+                board[0][2]!=' '){
+                    if(board[0][2]=='X') return "A";
+                    else return "B";
+                }
+        }
+        if(moves.size()==9) return "Draw";
+        return "Pending";
+    }
+};
